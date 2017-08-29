60420eb374360bb8a9925977e357870b091df762
Reda <redaaa99@hotmail.fr>
2017-08-30 00:05:35 +0100

    please be last

--- index.js
+++ index.js
@@ -169,8 +169,8 @@ app.get('/add', (req, res) => {
 });
 
 
-
+var port = process.env.PORT || 3000;
 // start the server
-app.listen(process.env.PORT || 3000, () => {
+app.listen(port, () => {
     console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
 });
