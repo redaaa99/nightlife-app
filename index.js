5b01ff4beb51097a9136d66ce1ca707548852612
Reda <redaaa99@hotmail.fr>
2017-08-29 23:59:49 +0100

    last deploy 3llah

--- index.js
+++ index.js
@@ -171,6 +171,6 @@ app.get('/add', (req, res) => {
 
 
 // start the server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
 });
